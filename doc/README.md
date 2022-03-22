6999b8f148d4dec6e32ccedd465011ee8cea9e53
b-vit <46031140+b-vit@users.noreply.github.com>
2022-03-22 20:27:06 +0100

    readme + types of RLG and DFA

--- doc/README.md
+++ doc/README.md
@@ -1,2 +1,17 @@
-# haskell-plg2nka
- 
+## plg2nka
+### A Haskell project
+The program converts Right Linear Grammar to a Nondeterministic Finite Automaton. 
+
+### Build and run
+Build using `make` and run using `./flp-fun21 [-i -1 -2] (input_file)`
+
+Example of an input file containing RLG G = (N,Sigma,P,S):
+``
+A,B
+a,b,c
+A
+A->aaB
+A->ccB
+B->bB
+B->#
+``
--- src/Types.hs
+++ src/Types.hs
@@ -1,7 +1,7 @@
 module Types where
 
--- Representation of the right linear grammar RLG G = (N,Sigma,P,S), where rules are list of tupples where one tupple represents one rule for example A -> B
-data RLG = RLG {nonterminals :: [Char], terminals :: [Char], rules :: [(Char,Char)], startingSymbol :: Char}
+-- Representation of the right linear grammar RLG G = (N,Sigma,P,S), where rules are list of tupples where one tupple represents one rule for example A -> aC
+data RLG = RLG {nonterminals :: [Char], terminals :: [Char], rules :: [(Char,[Char])], startingSymbol :: Char}
     deriving (Show)
 
 -- Representation of nondeterministic finite automaton DFA M = (Q, Sigma, Delta, q0, F), delta is d: Q x Sigma -> 2^Q
